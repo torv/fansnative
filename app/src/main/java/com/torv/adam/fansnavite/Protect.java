0675e2ff9febcc7fd90481a5b2e5611fbafc5d03
torv <aracclove@gmail.com>
2016-11-03 20:09:28 +0800

    inital project

---
+++ app/src/main/java/com/torv/adam/fansnavite/Protect.java
@@ -0,0 +1,19 @@
+package com.torv.adam.fansnavite;
+
+/**
+ * Created by AdamLi on 2016/11/3.
+ */
+
+public class Protect {
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
